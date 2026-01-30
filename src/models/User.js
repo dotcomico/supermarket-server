e6b5e164c32e7c78c784978f3635d53db3637465
dotan <dotcompanyofficial@gmail.com>
2026-01-30 13:17:42 +0200

    refactor: add address field

--- src/models/User.js
+++ src/models/User.js
@@ -2,13 +2,24 @@ import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
 import { ROLES } from "../config/constants.js";
 
-const User = sequelize.define('User', { 
-    username: {type: DataTypes.STRING , allowNull: false},
-    email: {type: DataTypes.STRING , allowNull: false ,unique:true, validate:{isEmail:true}},
-    password: {type: DataTypes.STRING , allowNull: false},
-    role:{
-       type: DataTypes.ENUM(...Object.values(ROLES)),
-       defaultValue: ROLES.CUSTOMER
-}
+const User = sequelize.define('User', {
+    username: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { isEmail: true } },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: {
+        type: DataTypes.ENUM(...Object.values(ROLES)),
+        defaultValue: ROLES.CUSTOMER
+    },
+    address: {
+        type: DataTypes.JSON,
+        allowNull: true,
+        defaultValue: {
+            street: "",
+            houseNumber: "",
+            apartment: "",
+            city: "",
+            zipCode: ""
+        }
+    }
 });
 export default User;
